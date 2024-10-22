d21cd77569a3dcc6c9833326b273a274117b971b
Minh Ngoc Nguyen <minhngocnguyen.403@gmail.com>
2024-10-23 02:04:20 +0700

    fix: docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 echo "Apply database migrations"
-python manage.py migrate
+python3 manage.py migrate
+
+echo "Starting collectstatic"
+python3 manage.py collectstatic
 
 echo "Starting server"
 gunicorn --bind 0.0.0.0:80 --access-logfile - detect_ai_backend.wsgi
