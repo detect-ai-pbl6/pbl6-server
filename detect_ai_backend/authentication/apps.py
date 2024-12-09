2566beff4bef35e6c4003e3c77131d87edccf601
Minh Ngoc Nguyen <minhngocnguyen.403@gmail.com>
2024-12-09 23:48:19 +0700

    fix: user avatar when login socials

--- detect_ai_backend/authentication/apps.py
+++ detect_ai_backend/authentication/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class RefreshTokensConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "detect_ai_backend.authentication"
+
+    def ready(self):
+        import detect_ai_backend.authentication.socials_signals  # noqa
+
+        return super().ready()
