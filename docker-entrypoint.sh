64e1134e3a6da8ba18c75f3eb09d43356eaab526
Minh Ngoc Nguyen <minhngocnguyen.403@gmail.com>
2024-12-14 16:08:16 +0700

    chore: update celery worker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,4 +8,4 @@ python3 manage.py collectstatic
 
 echo "Starting server & celery server"
 daphne -b 0.0.0.0 -p 80 detect_ai_backend.asgi:application &
-celery -A detect_ai_backend worker -l INFO --concurrency 4 -E -n backend_worker
+celery -A detect_ai_backend worker -l INFO --concurrency 4 -P solo -E -n backend_worker
